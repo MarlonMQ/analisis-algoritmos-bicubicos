609b7e8266b38711be01916b0693f221ee21f466
braunnymadrigal <braunnyjmb@gmail.com>
2025-11-08 17:21:42 -0600

    listo el codigo para generar Q
    solo falta correrlo

--- src/calculate_Q.py
+++ src/calculate_Q.py
@@ -1,63 +1,33 @@
-# from sewar.full_ref import uqi
-# from PIL import Image
-# import numpy as np
-
-# # Load grayscale images
-# img_ref = np.array(Image.open('img006.jpg').convert('L'))
-# img_a = np.array(Image.open('img010.jpg').convert('L'))
-# img_b = np.array(Image.open('img006.jpg').convert('L'))
-
-
-# # Compute UQI
-# score_dark = uqi(img_ref, img_a)
-# print("Score of A image:", score_dark)
-
-# score_dark = uqi(img_ref, img_b)
-# print("Score of B image:", score_dark)
+from sewar.full_ref import uqi
+from PIL import Image
+import numpy as np
+import pandas as pd
 
 from image_scaling import bspline_kernel
 from image_scaling import interpolation_kernel
 from image_scaling import scale
 
-from PIL import Image
-import numpy as np
+df = pd.read_csv("observaciones.csv")
+df = df.reset_index()
 
-img_ref = np.array(Image.open('img006.jpg').convert('L'), dtype=np.float32)
-img_a = np.array(Image.open('img010.jpg').convert('L'), dtype=np.float32)
+#-------------------------------------------------------------------------------
+for index, row in df.iterrows():
+    print(index + 1)
+    factor = int(row['FACTOR'][1:])
+    resolution = int(row['BLOQUE'])
+    img_number = int(row['IMAGEN'])
+    path = rf'C:\Users\braunny\Desktop\analisis-algoritmos-bicubicos\data\processed\bloque_{resolution}_bn\img{img_number:03}.jpg'
+    img_ref = np.array(Image.open(path).convert('L'), dtype=np.float32)
+    
+    if row['ALGORITMO'] == 'interpolation':
+        img_aux = scale(img_ref, int(resolution / factor), interpolation_kernel)
+        img_new = scale(img_aux, resolution, interpolation_kernel)   
+    else:
+        img_aux = scale(img_ref, int(resolution / factor), bspline_kernel)
+        img_new = scale(img_aux, resolution, bspline_kernel)
+        
+    q_index = uqi(img_ref, img_new)
+    df.loc[index, 'Q'] = q_index
+#-------------------------------------------------------------------------------
 
-img_b = scale(img_ref, 128, bspline_kernel)
-img_b = scale(img_b, 256, bspline_kernel)
-
-img_c = scale(img_ref, 64, bspline_kernel)
-img_c = scale(img_c, 256, bspline_kernel)
-
-from sewar.full_ref import uqi
-
-# Compute UQI
-score_a = uqi(img_ref, img_a)
-print("Score of A image:", score_a)
-
-score_b = uqi(img_ref, img_b)
-print("Score of B image:", score_b)
-
-score_c = uqi(img_ref, img_c)
-print("Score of C image:", score_c)
-
-
-
-
-
-
-
-
-img_b_uint8 = np.clip(img_b, 0, 255).astype(np.uint8)
-# Convert to PIL grayscale image
-image_b_pil = Image.fromarray(img_b_uint8, mode='L')  # 'L' = grayscale
-# Save or show
-image_b_pil.save('img_b_scaled.jpg')
-
-img_c_uint8 = np.clip(img_c, 0, 255).astype(np.uint8)
-# Convert to PIL grayscale image
-image_c_pil = Image.fromarray(img_c_uint8, mode='L')  # 'L' = grayscale
-# Save or show
-image_c_pil.save('img_c_scaled.jpg')
+df.to_csv('observaciones_con_Q.csv', index=False)
